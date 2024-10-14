d43ec8df3b73fa2232eb1a7c44e97e6497f06261
Hacha_tq55@student.itstep.org <Hacha_tq55@student.itstep.org>
2024-10-14 20:56:30 +0300

    Добавьте файлы проекта.

---
+++ SP_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SP_2
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            using (var udpClient = new UdpClient(8001))
+            {
+                var brodcastAddress = IPAddress.Parse("235.5.5.11");//хост для отправки данных
+
+                udpClient.JoinMulticastGroup(brodcastAddress, 50);
+                while (true)
+                {
+                    var result = await udpClient.ReceiveAsync();
+                    string message = Encoding.UTF8.GetString(result.Buffer);
+                    
+                    Console.WriteLine(message);
+                }
+
+                udpClient.DropMulticastGroup(brodcastAddress);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var messages = new string[] { "Жизнь — это то, что с тобой происходит, пока ты строишь планы. Джон Леннон",
+                "Стремитесь не к успеху, а к ценностям, которые он дает. Альберт Эйнштейн",
+                "Сначала мечты кажутся невозможными, затем неправдоподобными, а потом неизбежными. Кристофер Рив",
+                "Лучшая месть — огромный успех. Фрэнк Синатра",
+                "Талант — это дар, которому невозможно ни научить, ни научиться. Иммануил Кант",
+                "Единственный способ делать свою работу хорошо — это любить ее. Если ты еще не нашел свое любимое дело, продолжай искать. Стив Джобс"};
+            var brodcastAddress = IPAddress.Parse("235.5.5.11");
+            using var udpSender = new UdpClient();
+            Random rand = new Random();
+            
+            foreach (var message in messages)
+            {
+                Console.WriteLine($"A message is being sent: {rand.Next(messages.Length)}");
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                await udpSender.SendAsync(data, new IPEndPoint(brodcastAddress, 8001));
+                await Task.Delay(1000);
+            }
+            Console.ReadLine();
+        }
+    }
+}
